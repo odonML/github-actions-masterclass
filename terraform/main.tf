aebfc098af25d6246f1f448408b0cb0d4a9a0e67
odonML <odon_ml@outlook.com>
2026-04-19 23:02:07 -0600

    change perrmisions

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,6 +49,7 @@ resource "aws_instance" "web_server" {
               sudo systemctl start httpd
               sudo systemctl enable httpd
               sudo chown -R ec2-user:ec2-user /var/www/html
+              sudo chmod -R 755 /var/www/html
               EOF
 
   tags = {
